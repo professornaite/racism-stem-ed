6855303f6eb67a64f07657afbba314841e70eede
professornaite <professornaite@gmail.com>
2024-02-23 21:51:51 -0500

    api key update

--- code/pubmed/racism-bibliographic-test.R
+++ code/pubmed/racism-bibliographic-test.R
@@ -9,6 +9,8 @@ install.packages("pubmedR")
 # load libraries
 library(pubmedR)
 
-# api key
+# api key: https://www.ncbi.nlm.nih.gov/account/settings/
 api_key <- rstudioapi::askForSecret("pubmedapi")
 api_key
+
+query <- "bibliometric*[Title/Abstract] AND english[LA] AND Journal Article[PT] AND 2000:2020[DP]"
