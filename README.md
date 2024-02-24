2fdb64779f3791c8e604debefbb996eb2b2bbdac
professornaite <professornaite@gmail.com>
2024-02-23 22:11:11 -0500

    update readme

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Various explorations of race and racism in STEM education.
 
 ![](https://github.com/professornaite/racism-stem-ed/blob/main/output/plots-trends/racism-10year.jpeg?raw=false)
 
+Files in this project make use of the following packages, programs, and resources:
+
+  -- [pubmedR](https://cran.r-project.org/web/packages/pubmedR/index.html)
+  
+  -- [Google Trends](https://trends.google.com/)
+
 Project team: Nathan Alexander, Qyana Stewart, Basil Ghali
--- code/pubmed/racism-bibliographic-test.R
+++ code/pubmed/racism-bibliographic-test.R
@@ -13,4 +13,25 @@ library(pubmedR)
 api_key <- rstudioapi::askForSecret("pubmedapi")
 api_key
 
-query <- "bibliometric*[Title/Abstract] AND english[LA] AND Journal Article[PT] AND 2000:2020[DP]"
+# query articles
+  # written in English: English[LA]
+  # journal articles: Journal Article[PT]
+  # published in the last 10 years
+racism_query <- "racism*[Title/Abstract] AND english[LA] AND Journal Article[PT] AND 2013:2023[DP]"
+
+# return total documents retrieved
+racism_query_result <- pmQueryTotalCount(query = racism_query, api_key = api_key)
+racism_query_result$total_count
+
+# download a subset of the collection of documents
+D <- pmApiRequest(query = racism_query, limit = racism_query_result$total_count/7, api_key = NULL)
+
+D$query_translation
+
+
+# download subset and transform data
+install.packages("bibliometrix")
+library(bibliometrix)
+M <- convert2df(D, dbsource = "pubmed", format = "api")
+results <- biblioAnalysis(M)
+summary(results)
