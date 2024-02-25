d3f52f36acc3a0cc672a512b650d17bd5e40ecc2
professornaite <professornaite@gmail.com>
2024-02-25 14:38:10 -0500

    wos update

--- code/webofscience/step01-import.R
+++ code/webofscience/step01-import.R
@@ -1,1 +1,11 @@
 # step01-web of science
+# developer portal
+# code samples: https://github.com/clarivate?q=wosstarter&type=&language=&sort=
+# wos R interface (old): https://github.com/juba/rwos
+# search: https://www.google.com/search?client=firefox-b-1-d&q=web+of+science+in+R
+
+install.packages("devtools")  # if required
+devtools::install_github("juba/rwos")
+
+library(rwos)
+sid <- wos_authenticate()
