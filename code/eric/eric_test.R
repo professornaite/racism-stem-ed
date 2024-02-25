213cce75391d59bcc15b134e92097c1fa5dafd1f
Nathan Alexander <nathanalexander@Nathans-MacBook-Pro.local>
2024-02-25 13:50:25 -0500

    code update

--- code/eric/eric_test.R
+++ code/eric/eric_test.R
@@ -5,3 +5,4 @@ require(dplyr)
 
 racism_stem <- read_csv("https://api.ies.ed.gov/eric/?search=subject%3A%20%22racism%22%20AND%20subject%20%22stem%22&format=csv&rows=200")
 racism_stem
+View(racism_stem)
