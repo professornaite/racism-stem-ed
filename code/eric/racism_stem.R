3fb1cf44a5e7ee0940fd0031dbf491c4ce3ea137
professornaite <professornaite@gmail.com>
2024-03-25 10:13:17 -0400

    racism and stem tags

---
+++ code/eric/racism_stem.R
@@ -0,0 +1,11 @@
+# ERIC database API
+
+# test of race and racism in STEM tags
+
+install.packages("tidyverse")
+require(tidyverse)
+require(dplyr)
+
+racism_stem_v2 <- read.csv("https://api.ies.ed.gov/eric/?search=%22racism%22%20AND%20%22stem%22&format=csv&rows=2000")
+racism_stem_v2
+View(racism_stem_v2)
