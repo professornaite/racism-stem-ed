b1b5f665cf0ba3609e70cf775e1bd0578216d0fd
professornaite <professornaite@gmail.com>
2024-03-25 10:39:44 -0400

    quanteda update

--- code/eric/racism_stem.R
+++ code/eric/racism_stem.R
@@ -5,9 +5,6 @@
 require(quanteda)
 require(tidyverse)
 require(dplyr)
-install.packages("pdftools")
-install.packages("streamR")
-require(readtext)
 
 racism_stem_v2 <- read.csv("https://api.ies.ed.gov/eric/?search=%22racism%22%20AND%20%22stem%22&format=csv&rows=2000")
 racism_stem_v2
