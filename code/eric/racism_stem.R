af5fc6ab6bda0e318c8e672ee3d30b794729c5cb
professornaite <professornaite@gmail.com>
2024-03-25 10:26:04 -0400

    package updates

--- code/eric/racism_stem.R
+++ code/eric/racism_stem.R
@@ -2,11 +2,12 @@
 
 # test of race and racism in STEM tags
 
-install.packages("tidyverse")
-install.packages("quanteda")
-library(quanteda)
+require(quanteda)
 require(tidyverse)
 require(dplyr)
+install.packages("pdftools")
+install.packages("streamR")
+require(readtext)
 
 racism_stem_v2 <- read.csv("https://api.ies.ed.gov/eric/?search=%22racism%22%20AND%20%22stem%22&format=csv&rows=2000")
 racism_stem_v2
