8a555b010e2747200c2f5a143b54a472e6827521
professornaite <professornaite@gmail.com>
2024-03-25 10:49:21 -0400

    code update

--- code/eric/racism_stem.R
+++ code/eric/racism_stem.R
@@ -10,3 +10,24 @@ racism_stem_v2 <- read.csv("https://api.ies.ed.gov/eric/?search=%22racism%22%20A
 racism_stem_v2
 View(racism_stem_v2)
 
+df <- as_tibble(racism_stem_v2)
+str(df)
+dim(df)
+
+# corpus of article titles
+print(df$title)
+corp_title <- corpus(df$title)
+print(corp_title)
+summary(corp_title)
+
+# corpus of descriptions
+corp_description <- corpus(df$description)
+summary(corp_description)
+
+# tokens of descriptions
+toks_description <- tokens(df$description)
+toks_description
+
+  # concordonance view
+  kw_description <- kwic(toks_description, pattern = "rac*")
+  head(kw_description)
