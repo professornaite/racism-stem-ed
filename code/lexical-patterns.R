0155c34cced29512db32341a20cc43bf17cd00b5
professornaite <professornaite@gmail.com>
2024-07-05 15:02:16 -0400

    lexical ptn

--- code/lexical-patterns.R
+++ code/lexical-patterns.R
@@ -7,7 +7,18 @@ df1_2018_2023 <- df1 %>%
   filter(publicationdateyear > 2018 & publicationdateyear < 2024) 
 
 df1_2018_2023_corp_abstract <- corpus(df1_2018_2023$description)
-  
+
+library(quanteda)
+library("quanteda.textmodels")
+library("quanteda.textstats")
+library("quanteda.textplots")
+M1_corp_abstract <- corpus(M1_dup$AB)
+M1_corp
+toks_M1_corp_abstract <- corpus_subset(M1_corp_abstract) %>% 
+  tokens()
+kwic(toks_M1_corp_abstract, pattern = "racism") %>% 
+  textplot_xray()
+
 toks_corp_racism_and_stem_abstract_df1 <- corpus_subset(df1_2018_2023_corp_abstract) %>% 
   tokens()
   
