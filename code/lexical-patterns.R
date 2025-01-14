e9d4addcb00f10388f55b49732f95e132a97c24f
professornaite <professornaite@gmail.com>
2025-01-14 10:24:20 -0500

    lexical patterns

--- code/lexical-patterns.R
+++ code/lexical-patterns.R
@@ -8,10 +8,20 @@ df1_2018_2023 <- df1 %>%
 
 df1_2018_2023_corp_abstract <- corpus(df1_2018_2023$description)
 
+# LEXICAL PATTERNS
+# keywords in context
+
 library(quanteda)
 library("quanteda.textmodels")
 library("quanteda.textstats")
 library("quanteda.textplots")
+M4_abstract <- corpus(M4$AB)
+M4_abstract
+toks_M4_abstract <- corpus_subset(M4_abstract) %>% 
+  tokens()
+kwic(toks_M4_abstract, pattern = "racism") %>% 
+  textplot_xray()
+
 M1_corp_abstract <- corpus(M1_dup$AB)
 M1_corp
 toks_M1_corp_abstract <- corpus_subset(M1_corp_abstract) %>% 
